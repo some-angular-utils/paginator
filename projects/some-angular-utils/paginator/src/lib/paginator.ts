39b8181f30293f4b23856c5924326c4b89f98aff
José de los santos <delossantosramosjose@gmail.com>
2026-01-06 20:51:32 +0100

    add initial library

---
+++ projects/some-angular-utils/paginator/src/lib/paginator.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sau-paginator',
+  imports: [],
+  template: `
+    <p>
+      paginator works!
+    </p>
+  `,
+  styles: ``,
+})
+export class SAUPaginatorModule {
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SAUPaginatorModule } from '@some-angular-utils/paginator';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SAUPaginatorModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
