d3767b0f55a2e962f4344a1d91381728ecea85ee
José de los santos <delossantosramosjose@gmail.com>
2026-02-01 10:34:35 +0100

    Change default variable values to :host

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { SAUPaginatorModule } from '@some-angular-utils/paginator';
 
 @Component({
   selector: 'app-root',
   imports: [SAUPaginatorModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class App {
   totalPages: number = 0;
   currentPage: number = 0;
 
-  ngOnInit(){
+  ngOnInit() {
     this.totalPages = 101
-    this.currentPage= 1
+    this.currentPage = 1
   }
 
   onPageChange(nuevaPagina: number) {
