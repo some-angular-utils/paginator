6795817c8405055191b7f5a73c8f87d5badb6caf
José de los santos <delossantosramosjose@gmail.com>
2026-03-14 14:35:20 +0100

    delete build:gh-pages

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,23 +1,11 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { SAUPaginatorModule } from '@some-angular-utils/paginator';
 
 @Component({
   selector: 'app-root',
-  imports: [SAUPaginatorModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   encapsulation: ViewEncapsulation.None
 })
 export class App {
-  totalPages: number = 0;
-  currentPage: number = 0;
 
-  ngOnInit() {
-    this.totalPages = 101
-    this.currentPage = 1
-  }
-
-  onPageChange(nuevaPagina: number) {
-    this.currentPage = nuevaPagina;
-  }
 }
