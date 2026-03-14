db5cb3437fc21972f2090415f620526933c2b3f3
José de los santos <delossantosramosjose@gmail.com>
2026-03-14 15:14:37 +0100

    delete budges in angular.json

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  encapsulation: ViewEncapsulation.None
 })
 export class App {
 
